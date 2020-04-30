8e52e6e139b91fe6fafdb4a6c75480bdf4bbea10
Haden <hadenhiles@gmail.com>
2020-04-30 13:29:42 -0400

    Fix reference to redirect_uri in login button

--- plugins/default/oauth/login.php
+++ plugins/default/oauth/login.php
@@ -4,7 +4,7 @@
 ?>
 
 
-<a href="<?=$settings->wp_consumer_authorization_url?>?response_type=code&client_id=<?=$settings->wp_consumer_key?>&redirect_uri=<?=$settings->wp_redirect_uri?>" class="btn btn-block btn-social btn-wordpress btn-sm">
+<a href="<?=$settings->wp_consumer_authorization_url?>?response_type=code&client_id=<?=$settings->wp_consumer_key?>&redirect_uri=<?=ossn_site_url('oauth_login/wordpress');?>" class="btn btn-block btn-social btn-wordpress btn-sm">
   <span class="fa fa-wordpress"></span>
   <?php echo ossn_print('oauth:login:with:wordpress');?>
 </a>
